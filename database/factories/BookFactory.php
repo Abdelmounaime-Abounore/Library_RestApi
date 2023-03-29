43a28c5620c5db2ef765cdb046dff30dcaaacd1f
Abdelmounaime-Abounore <abdelmounaime.abounore@gmail.com>
2023-03-29 02:59:16 +0000

    add Spatie

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->name(),
+            'author' => fake()->name(),	
+            'collection' => fake()->name(),	
+            'isbn' => fake()->name(),
+            'publish_date' => fake()->date(),				
+            'pages_number' => fake()->numberBetween(1,1000),	
+            'location' => fake()->name(),	
+            'content' => fake()->paragraph(3, true),	
+            'category_id' => fake()->numberBetween(1, 1000)
+        ];
+    }
+}
